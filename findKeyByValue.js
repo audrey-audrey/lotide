cdc723209a0206e53d3728bcc05b7523ef33ee58
audrey-audrey <audreyuu@gmail.com>
2020-10-28 23:46:54 +0000

    Added findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+// assertEqual
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// findKeyByValue function
+const findKeyByValue = function(list, value) {
+
+  let result;
+
+  for (const item in list) {
+    if (list[item] === value) {
+      result = item;
+    }
+  }
+  return result;
+
+};
+
+// Test case
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

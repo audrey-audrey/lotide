c928fd004bc9d4cb2a6d327d0d576e94326f2634
audrey-audrey <audreyuu@gmail.com>
2020-10-28 18:26:01 +0000

    Fixed linting errors

--- middle.js
+++ middle.js
@@ -1,5 +1,5 @@
 // eqArrays function
-const eqArrays = function (arrayFirst, arraySecond) {
+const eqArrays = function(arrayFirst, arraySecond) {
 
   let output = true;
   
@@ -11,29 +11,29 @@ const eqArrays = function (arrayFirst, arraySecond) {
   
   return output;
   
-  };
+};
   
-  // assertArraysEqual function
-  const assertArraysEqual = function (inputOne, inputTwo) {
+// assertArraysEqual function
+const assertArraysEqual = function(inputOne, inputTwo) {
   
   let result = eqArrays(inputOne, inputTwo);
   
-    if (result === true) {
-      console.log(`✅✅✅ Assertion passed: ${inputOne} === ${inputTwo}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion failed: ${inputOne} !== ${inputTwo}`);
-    }
-  
+  if (result === true) {
+    console.log(`✅✅✅ Assertion passed: ${inputOne} === ${inputTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${inputOne} !== ${inputTwo}`);
   }
+  
+};
 
-  // middle function 
-const middle = function (array) {
+// middle function
+const middle = function(array) {
 
   // Empty array
   let retArray = [];
 
   // Middle element of the array
-  let middleNum = Math.floor((array.length)/2)
+  let middleNum = Math.floor((array.length) / 2);
 
   if ((array.length > 2) && (array.length % 2 === 0)) {
     retArray.push(array[middleNum - 1], array[middleNum]);
@@ -42,19 +42,19 @@ const middle = function (array) {
     retArray.push(array[middleNum]);
   }
   
-  console.log(retArray)
+  console.log(retArray);
   return retArray;
-}
+};
 
 // Test case 1
-middle([1]) // => []
-middle([1, 2]) // => []
+middle([1]); // => []
+middle([1, 2]); // => []
 
 // Test case 2
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
+middle([1, 2, 3]); // => [2]
+middle([1, 2, 3, 4, 5]); // => [3]
 
 // Test case 3
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+middle([1, 2, 3, 4]); // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
 

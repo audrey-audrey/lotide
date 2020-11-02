bad6cab7d7ad0598e082bb3ee3aa4fc2137d800d
audrey-audrey <audreyuu@gmail.com>
2020-11-02 06:02:35 +0000

    Converted testing using Mocha and Chai based test code

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,17 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-// TEST CODE
-assertEqual(head([5]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+
+});
